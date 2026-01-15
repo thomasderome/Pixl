199b425546ca953e9882bcbf0c22bca285aff489
Belzébuth <mateo.busalli@epitech.eu>
2026-01-15 11:16:38 +0100

    chaispasgros

--- Pixl/Assets/Scripts/UI/SelectScript.cs
+++ Pixl/Assets/Scripts/UI/SelectScript.cs
@@ -5,14 +5,23 @@ public class SelectScript : MonoBehaviour
 {
     public GameObject selectionPanel;
     private Image  currentTargetImage;
-    private Image defaultImage;
+    public Image defaultImage;
     [SerializeField] private Image[] loadoutSlots;
     private Sprite defaultSprite;
+    private GameObject[] selectedGadgets = new GameObject[3];
+    [SerializeField] private GameObject[] allGadgets;
+    private GameObject randomButton;
     
     void Start()
     {
         selectionPanel.SetActive(false);
         defaultSprite=loadoutSlots[0].sprite;
+        randomButton = defaultImage.gameObject;
+        for (int i = 0; i < selectedGadgets.Length; i++)
+        {
+            selectedGadgets[i] = randomButton;
+            loadoutSlots[i].sprite = randomButton.GetComponent<Image>().sprite;
+        }
     }
     
     public void OpenSelect()
@@ -37,11 +46,30 @@ public class SelectScript : MonoBehaviour
         
         Image buttonImage = button.GetComponent<Image>();
         currentTargetImage.sprite = buttonImage.sprite;
+        int slotIndex = System.Array.IndexOf(loadoutSlots, currentTargetImage);
+        selectedGadgets[slotIndex]=button.gameObject;
         selectionPanel.SetActive(false);
-        Debug.Log(buttonImage.name);
         
     }
 
+    public void ConfirmSelection()
+    {
+        string finalLoadout = "";
+        for (int i = 0; i < selectedGadgets.Length; i++)
+        {
+            GameObject gadget = selectedGadgets[i];
+            int slot = i + 1;
+            if (gadget.name == "Random")
+            {
+                GameObject randomPick = allGadgets[Random.Range(0, allGadgets.Length)];
+                gadget = randomPick;
+            }
+            finalLoadout+= $"Slot {i+1}: {gadget.name} ";
+        }
+        Debug.Log(finalLoadout);
+        
+    }
+    
     public void ResetToDefault()
     {
         foreach (Image slot in loadoutSlots)
